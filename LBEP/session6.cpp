f02e32ec6175c0b4c2e86ac1455e2ef87aec4736
quanghoa.trinh@gmail.com <quanghoa.trinh@gmail.com>
2019-08-02 11:28:00 +0700

    session 6

---
+++ LBEP/session6.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+	int x[10];
+
+	x[0] = 1;
+	x[1] = 5;
+
+	x[9] = 100;
+
+	int y ;
+	y = 50;
+
+	printf("gia tri thu 9 la %d\n",x[9]);
+	printf("gia tri cua y la %d\n",y);
+
+	
+
+
+	return 0;
+}
